9963920448c0f407f852d9035b96b0f97ff92045
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-03 19:38:58 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -5,7 +5,7 @@ directors_database
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+    pp directors_database 
 end
 
 def print_first_directors_movie_titles
